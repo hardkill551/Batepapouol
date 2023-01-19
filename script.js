aa18eb3212114ca4c48c431bcfff793326eebc2b
HardKILL <hardkill194@gmail.com>
2023-01-18 22:50:48 -0300

    Chat feito

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 let person;
 let mensagem = document.querySelector("ul");
+let mensagemAnterior;
+let elementoqueaparece;
 
-
-
-function mostrarmensagens(resposta){
-    console.log(resposta)
-    console.log(resposta.data)
-    console.log(resposta.data[0].type)
-    console.log(resposta.data.length)
-    for (let i = 0; i < resposta.data.length; i++){
-    if (resposta.data[i].type == "status"){
-    mensagem.innerHTML += `<li class="status"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> ${(resposta.data[i].text)}</li>`
-    }
-    if (resposta.data[i].type == "message"){
-        mensagem.innerHTML += `<li class="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> para <em>${(resposta.data[i].to)}: </em>${(resposta.data[i].text)}</li>`
-    }
-    if (resposta.data[i].type == "private_message"){
-        mensagem.innerHTML += `<li class="private_message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> reservadamente para <em>${(resposta.data[i].to)}: </em>${(resposta.data[i].text)}</li>`
-    }
+function mostrarMensagens(resposta){
+    const resposta1 = resposta.data[99].time
+    if(mensagemAnterior!=resposta1){
+        if (resposta.data[99].type == "status"){
+            mensagem.innerHTML += `<li class="status"><span>${(resposta.data[99].time)} </span><em>${(resposta.data[99].from)}</em> ${(resposta.data[99].text)}</li>`
+        }
+        if (resposta.data[99].type == "message"){
+            mensagem.innerHTML += `<li class="message"><span>${(resposta.data[99].time)} </span><em>${(resposta.data[99].from)}</em> para <em>${(resposta.data[99].to)}: </em>${(resposta.data[99].text)}</li>`
+        }
+        if (resposta.data[99].type == "private_message"){
+            if (person.name == resposta.data[99].from || person.name == resposta.data[99].to){
+            mensagem.innerHTML += `<li class="private_message"><span>${(resposta.data[99].time)} </span><em>${(resposta.data[99].from)}</em> reservadamente para <em>${(resposta.data[99].to)}: </em>${(resposta.data[99].text)}</li>`
+            }
+        }
+        mensagemAnterior = resposta1
+        elementoqueaparece = document.querySelector('ul li:last-child');
+        elementoqueaparece.scrollIntoView();
     }
 }
-
 function coletarMensagens(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-    promise.then(mostrarmensagens)
+    promise.then(mostrarMensagens)
 }
 
 function manterConexão(){
@@ -33,6 +34,7 @@ function manterConexão(){
 function entrou(){
     alert("Entrou")
     coletarMensagens()
+    let y = setInterval(coletarMensagens, 3000)
     let x = setInterval(manterConexão, 5000)
 }
 function naoEntrou(){
