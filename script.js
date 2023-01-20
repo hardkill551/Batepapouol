71e2dcc06d37004b580496ca9afac4e4b1163def
HardKILL <hardkill194@gmail.com>
2023-01-20 12:59:22 -0300

    tentando desbugar 6

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let mensagemAnterior = [];
 let elementoqueaparece;
 
 function reload(){
-    alert("Você não está mais logado")
-    window.location.reload()
+    alert("Você não está mais logado");
+    window.location.reload();
 }
 
 function enviarMensagens(){
@@ -14,11 +14,11 @@ function enviarMensagens(){
         to: "Todos",
         text: document.querySelector("input").value,
         type: "message"
-    }
+    };
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", EnvioDaMensagem);
     promise.then(coletarMensagens);
     promise.catch(reload);
-    document.querySelector("input").value = ""
+    document.querySelector("input").value = "";
 }
 
 function mostrarMensagens(resposta){
@@ -26,35 +26,35 @@ function mostrarMensagens(resposta){
     while (deletandoMensagens.firstChild) {
     deletandoMensagens.removeChild(deletandoMensagens.firstChild);
     }
-    for (let i =0;i<resposta.data.length-1;i++){
+    for (let i =0;i<resposta.data.length;i++){
     if (resposta.data[i].type == "status"){
-        mensagem.innerHTML += `<li class="status" data-test="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> ${(resposta.data[i].text)}</li>`
+        mensagem.innerHTML += `<li class="status" data-test="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> ${(resposta.data[i].text)}</li>`;
     }
     if (resposta.data[i].type == "message"){
-        mensagem.innerHTML += `<li class="message" data-test="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> para <em>${(resposta.data[i].to)}: </em>${(resposta.data[i].text)}</li>`
+        mensagem.innerHTML += `<li class="message" data-test="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> para <em>${(resposta.data[i].to)}: </em>${(resposta.data[i].text)}</li>`;
     }
     if (resposta.data[i].type == "private_message"){
         if (person.name == resposta.data[i].from || person.name == resposta.data[i].to){
-        mensagem.innerHTML += `<li class="private_message" data-test="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> reservadamente para <em>${(resposta.data[i].to)}: </em>${(resposta.data[i].text)}</li>`
+        mensagem.innerHTML += `<li class="private_message" data-test="message"><span>${(resposta.data[i].time)} </span><em>${(resposta.data[i].from)}</em> reservadamente para <em>${(resposta.data[i].to)}: </em>${(resposta.data[i].text)}</li>`;
         }
     }
     }
     if (mensagemAnterior[0] !== resposta.data[99].time || mensagemAnterior[1] !== resposta.data[99].from || mensagemAnterior[2] !== resposta.data[99].text){
         elementoqueaparece = document.querySelector('ul li:last-child');
         elementoqueaparece.scrollIntoView();
-        mensagemAnterior[0]=resposta.data[99].time
-        mensagemAnterior[1]=resposta.data[99].from
-        mensagemAnterior[2]=resposta.data[99].text
+        mensagemAnterior[0]=resposta.data[99].time;
+        mensagemAnterior[1]=resposta.data[99].from;
+        mensagemAnterior[2]=resposta.data[99].text;
     }
 }
 
 function coletarMensagens(){
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-    promise.then(mostrarMensagens)
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(mostrarMensagens);
 }
 
 function manterConexão(){
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", person)
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", person);
 }
 
 function entrou(resposta){
@@ -66,18 +66,18 @@ function entrou(resposta){
 }
 function naoEntrou(resposta){
     if (resposta.response.status === 400){
-    const member = prompt("Nome de usuário já existe, escolha outro")
-    person.name = member
-    entrarNaSala(resposta)
+    const member = prompt("Nome de usuário já existe, escolha outro");
+    person.name = member;
+    entrarNaSala(resposta);
 }
 }
 
 function entrarNaSala(){
-    const member = prompt("Qual seu nome?")
-    person = {name: member}
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", person)
-        promise.then(entrou)
-        promise.catch(naoEntrou)
+    const member = prompt("Qual seu nome?");
+    person = {name: member};
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", person);
+        promise.then(entrou);
+        promise.catch(naoEntrou);
 }
 
-entrarNaSala()
+entrarNaSala();
